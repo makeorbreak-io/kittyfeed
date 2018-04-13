07b9ebf05bb52e70ae37df5d560110b472bbc0fb
João Freitas <1160907@isep.ipp.pt>
2018-04-13 17:57:21 +0100

    Added listeners for feed me button on FeedMeActivity
    Added listeners for feed me button on FeedMeActivity

--- KittyFeed/app/src/main/java/zindeiros/kittyfeed/FeedMeNowActivity.java
+++ KittyFeed/app/src/main/java/zindeiros/kittyfeed/FeedMeNowActivity.java
@@ -1,7 +1,11 @@
 package zindeiros.kittyfeed;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class FeedMeNowActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class FeedMeNowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedmenow);
+        configureFeedMeButton();
+    }
+    private void configureFeedMeButton(){
+        ImageButton feedMeButton=findViewById(R.id.imageButton);
+        feedMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSadKittyImageView().setVisibility(View.INVISIBLE);
+                getHappyKittyImageView().setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    private ImageView getSadKittyImageView(){
+        ImageView sadKittyImageView=(ImageView)findViewById(R.id.sadKitty);
+        return sadKittyImageView;
+    }
+    private ImageView getHappyKittyImageView(){
+        ImageView happyKittyImageView=(ImageView)findViewById(R.id.happyKitty);
+        return happyKittyImageView;
     }
 }
