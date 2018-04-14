c0de76be9e8a03841efd7ae907aa166b57136c18
Rita Gonçalves <ritagsoraia@gmail.com>
2018-04-14 15:54:43 +0100

    Added UI improvements and navigation bar (menu)
    Added UI improvements and navigation bar (menu)

--- KittyFeed/app/src/main/java/zindeiros/kittyfeed/FeedMeNowActivity.java
+++ KittyFeed/app/src/main/java/zindeiros/kittyfeed/FeedMeNowActivity.java
@@ -10,13 +10,20 @@ import android.widget.ImageView;
 public class FeedMeNowActivity extends AppCompatActivity {
 
     @Override
+    /**
+     * Creates an instance of FeedMeNowActivity.
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedmenow);
         configureFeedMeButton();
     }
-    private void configureFeedMeButton(){
-        ImageButton feedMeButton=findViewById(R.id.imageButton);
+
+    /**
+     * Configures the action listener for the "feed me" button.
+     */
+    private void configureFeedMeButton() {
+        ImageButton feedMeButton = findViewById(R.id.imageButton);
         feedMeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,23 +31,41 @@ public class FeedMeNowActivity extends AppCompatActivity {
             }
         });
     }
-    private ImageView getSadKittyImageView(){
-        ImageView sadKittyImageView=(ImageView)findViewById(R.id.sadKitty);
+
+    /**
+     * Gets the sad kitty image view.
+     *
+     * @return the sad kitty image view
+     */
+    private ImageView getSadKittyImageView() {
+        ImageView sadKittyImageView = (ImageView) findViewById(R.id.sadKitty);
         return sadKittyImageView;
     }
-    private ImageView getHappyKittyImageView(){
-        ImageView happyKittyImageView=(ImageView)findViewById(R.id.happyKitty);
+
+    /**
+     * Gets the happy kitty image view.
+     *
+     * @return the happy kitty image view
+     */
+    private ImageView getHappyKittyImageView() {
+        ImageView happyKittyImageView = (ImageView) findViewById(R.id.happyKitty);
         return happyKittyImageView;
     }
 
-    private void changeKittyMood(){
+    /**
+     * Changes the mood of the kitty from sad to happy, sleeps n milliseconds and changes the mood
+     * once again, from happy to sad.
+     */
+    private void changeKittyMood() {
+        int n = 3000;
         getSadKittyImageView().setVisibility(View.INVISIBLE);
         getHappyKittyImageView().setVisibility(View.VISIBLE);
         try {
-            Thread.sleep(3000);
-        } catch (Exception ex){
+            Thread.sleep(n);
+        } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
+        getHappyKittyImageView().setVisibility(View.INVISIBLE);
         getSadKittyImageView().setVisibility(View.VISIBLE);
     }
 }
